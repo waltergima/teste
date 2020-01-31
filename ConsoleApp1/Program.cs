62b800194806a3a902dcd92bc8cb6303cd9c8fc2
Walter Gima <walter_gima@hotmail.com>
2020-01-31 16:28:45 -0200

    teste

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public String nome;
+        static void Main(string[] args)
+        {
+            //teste ddddddddddddddddddddddddddddd fffff plinio teste
+            //teste  teste testeDLP
+        }
+    }
+}
